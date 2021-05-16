be03936336f5e06d866cc8c51d4195577a322034
Kwaku Boamah <kwaku.boamah@petraonline.com>
2021-05-16 18:39:30 +0000

    project structure

---
+++ src/apps/Dynamo.Browser/ClientApp/src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <p>Dynamo App</p>
+      <div></div>
+    </div>
+  );
+}
+
+export default App;
